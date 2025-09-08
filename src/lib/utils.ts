50e716ff76e115011f1fb3132380ab07aae2f4e4
peterkudelas@gmail.com <peterkudelas@gmail.com>
2025-09-08 21:28:10 +0200

    gfm bitches

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import rehypeExternalLinks from "rehype-external-links";
 import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { twMerge } from "tailwind-merge";
@@ -13,6 +14,7 @@ export function cn(...inputs: ClassValue[]) {
 export async function markdownToHtml(markdown: string) {
   const result = await unified()
     .use(remarkParse)
+    .use(remarkGfm)
     .use(remarkRehype)
     .use(rehypeExternalLinks, { target: "_blank", rel: ["nofollow"] })
     .use(rehypeStringify)
