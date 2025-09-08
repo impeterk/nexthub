5e679573c2a52feaf183d0d7f141e125b06f0172
peterkudelas@gmail.com <peterkudelas@gmail.com>
2025-09-08 19:59:28 +0200

    External nocturna;

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,21 @@
 import { type ClassValue, clsx } from "clsx";
-import { remark } from "remark";
-import html from "remark-html";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
 import { twMerge } from "tailwind-merge";
+import { unified } from "unified";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString();
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeExternalLinks, { target: "_blank", rel: ["nofollow"] })
+    .use(rehypeStringify)
+    .process(markdown);
+  return result.value;
 }
