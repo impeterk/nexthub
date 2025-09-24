9297a50c7d8c2f049cd58c1bbcb2976b9507b779
peterkudelas@gmail.com <peterkudelas@gmail.com>
2025-09-25 00:00:33 +0200

    wa

--- next.config.ts
+++ next.config.ts
@@ -9,19 +9,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
         hostname: "peterk.dev",
       },
       {
         protocol: "https",
         hostname: "next.peterk.dev",
-      },
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
       },
     ],
   },
